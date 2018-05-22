044f4dfb3582724ce5f900e742fce2f631d6f450
Lorenzo Capocchiano <l.capocchiano@studenti.uniba.it>
2018-05-22 13:02:11 +0200

    revision

--- src/database/TableData.java
+++ src/database/TableData.java
@@ -15,7 +15,7 @@ import database.TableSchema.Column;
 
 public class TableData {
 
-	DbAccess db;
+	static DbAccess db;
 
 	public TableData(DbAccess db) {
 		this.db = db;
@@ -24,7 +24,6 @@ public class TableData {
 	public List<Example> getDistinctTransazioni(String table) throws SQLException, EmptySetException {
 		List<Example> temp = new ArrayList<Example>();
 		Connection conn = db.getConnection();
-		conn.isValid(10);
 		try {
 			Statement s = conn.createStatement();
 			String query = "SELECT * " + "FROM " + table;
@@ -50,35 +49,24 @@ public class TableData {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-
 		return temp;
 	}
 
 	public Set<Object> getDistinctColumnValues(String table, Column column) throws SQLException {
 		Set<Object> temp = new TreeSet<Object>();
-		try {
-			db.initConnection();
-			Connection conn = db.getConnection();
-			conn.isValid(10);
-			Statement s = conn.createStatement();
-			String query = "SELECT " + column.getColumnName() + " FROM " + table + " ORDER BY "
-					+ column.getColumnName();
-			ResultSet res = s.executeQuery(query);
-			while (res.next()) {
-				if (column.isNumber()) {
-					res.getDouble(column.getColumnName());
-				} else {
-					res.getString(column.getColumnName());
-				}
+		Connection conn = db.getConnection();
+		Statement s = conn.createStatement();
+		String query = "SELECT " + column.getColumnName() + " FROM " + table + " ORDER BY " + column.getColumnName();
+		ResultSet res = s.executeQuery(query);
+		while (res.next()) {
+			if (column.isNumber()) {
 
+				temp.add(res.getDouble(column.getColumnName()));
+			} else {
+				temp.add(res.getString(column.getColumnName()));
 			}
-		} catch (DatabaseConnectionException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
 		}
-
 		return temp;
-
 	}
 
 	public Object getAggregateColumnValue(String table, Column column, QUERY_TYPE aggregate)
@@ -92,7 +80,7 @@ public class TableData {
 			String query = "SELECT " + aggregate.toString() + "(" + column.getColumnName() + ") FROM " + table;
 			ResultSet res = s.executeQuery(query);
 			if (res.next()) {
-				return res.getObject(column.getColumnName());
+				return res.getString(1);
 			} else {
 				throw new NoValueException();
 			}
@@ -100,9 +88,7 @@ public class TableData {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
 		return temp;
 		// to do
 	}
-
 }
